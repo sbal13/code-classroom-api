cd96285a73c711922fdead83192c95902940f52d
sbal13 <sbal13@gmail.com>
2019-08-30 10:11:00 -0400

    sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,14 @@ class UsersController < ApplicationController
     user = User.order('RANDOM()').first
     render json: user
   end
+
+  def create
+    user = User.find_or_create_by(username: params[:username])
+
+    if user
+      render json: user
+    else
+      render json: {errors: "ya dun goofed!"}
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :submissions
+
+  validates :username, presence: true
   
-  has_secure_password
+  # has_secure_password
 end
